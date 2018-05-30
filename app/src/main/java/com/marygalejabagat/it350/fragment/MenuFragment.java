30d46b0ac14ee382c33d516c87d357800348ff4d
gale <mary.jabagat@ripeconcepts.com>
2018-05-30 16:04:45 +0800

    add fragment redirect

--- app/src/main/java/com/marygalejabagat/it350/fragment/MenuFragment.java
+++ app/src/main/java/com/marygalejabagat/it350/fragment/MenuFragment.java
@@ -3,17 +3,23 @@ package com.marygalejabagat.it350.fragment;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.marygalejabagat.it350.R;
 
 public class MenuFragment extends DialogFragment {
 
-    private EditText mEditText;
+    View view;
+    private Button btnStart;
+    private Button btnStop;
+    private Button btnEdit;
+    private Button btnDelete;
 
     public MenuFragment() {
         // Empty constructor is required for DialogFragment
@@ -32,7 +38,43 @@ public class MenuFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pop_menu, container);
+
+           view = inflater.inflate(R.layout.pop_menu, container);
+           btnStart = (Button) view.findViewById(R.id.btnStart);
+           btnStop = (Button) view.findViewById(R.id.btnStop);
+           btnEdit = (Button) view.findViewById(R.id.btnEdit);
+           btnDelete = (Button) view.findViewById(R.id.btnDelete);
+           btnStart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("START SURVEY", "THIS");
+                }
+
+            });
+
+            btnStop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("STOP SURVEY", "THIS");
+                }
+
+           });
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("EDIT SURVEY", "THIS");
+                }
+
+           });
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("DELETE SURVEY", "THIS");
+                }
+
+            });
+        return view;
+
     }
 
     @Override
--- app/src/main/java/com/marygalejabagat/it350/fragment/SurveyFragment.java
+++ app/src/main/java/com/marygalejabagat/it350/fragment/SurveyFragment.java
@@ -18,6 +18,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupMenu;
@@ -52,9 +53,10 @@ public class SurveyFragment extends Fragment{
     public static ProgressDialog pd;
     private ActionMode currentActionMode;
     private int currentListItemIndex;
-
+    private Button btnStart;
 
     View view;
+    View menuView;
 
     public SurveyFragment() {
         // Required empty public constructor
@@ -71,6 +73,7 @@ public class SurveyFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_survey, container, false);
+        menuView = inflater.inflate(R.layout.pop_menu, container, false);
 
         listView = (ListView) view.findViewById(R.id.ListSurvey);
         adapter = new SurveyListAdapter(getActivity(), survey_list);
@@ -80,91 +83,14 @@ public class SurveyFragment extends Fragment{
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getContext(), "Long CLICK", Toast.LENGTH_SHORT).show();
                 showEditDialog();
-              /*  PopupMenu popup = new PopupMenu (view.getContext(),view, Gravity.CENTER);
-                popup.inflate(R.menu.action_survey_menu);
-                popup.show();*/
-               /* if (currentActionMode != null) { return false; }
-                currentListItemIndex = position;
-                currentActionMode = startActionMode(modeCallBack);
-                currentActionMode= getActivity().startActionMode(modeCallBack);
-                view.setSelected(true);*/
                 return true;
             }
         });
 
         Log.e(TAG, "LOGS here");
         loadSurvey();
-        /*listView.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                *//*displayPopupWindow();*//*
-                PopupMenu popup = new PopupMenu (view.getContext(),listView);
-                popup.inflate(R.menu.action_survey_menu);
-                popup.show();
-                *//*popup.getMenuInflater()*//*
-
-            }
-        });*/
-
-        /*listView.setOnItemClickListener(new OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-                Log.e("CLICK ON", "LISTVIEW");
-                popMenu();
-            }
-
-        });
-        listView.setOnClickListener(View.OnClickListener);
-        listView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(view.getContext(), v);
-                popup.setOnMenuItemClickListener(PopupMenu popup);
-                popup.inflate(R.menu.action_survey_menu);
-                popup.show();
-            }
-        });
-        listView.setOnItemClickListener(new OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-                ItemClicked item = adapter.getItemAtPosition(position);
-                Log.e("CLICK ON", "LISTVIEW");
-                PopupMenu popup = new PopupMenu(view.getContext(), view);
-                popup.setOnMenuItemClickListener(view.getDisplay());
-                popup.inflate(R.menu.action_survey_menu);
-                MenuInflater inflater = popup.getMenuInflater();
-                inflater.inflate(R.menu.action_survey_menu, popup.getMenu());
-                popup.show();
-                popMenu();
-                displayPopupWindow(view);
-            }
-
-        });*/
-
-        /*listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                if (currentActionMode != null) { return false; }
-                currentListItemIndex = position;
-                currentActionMode = startActionMode(modeCallBack);
-                currentActionMode = SurveyFragment.this.getActivity().startActionMode(modeCallBack);
-                view.setSelected(true);
-                return true;
-            }
-        });*/
         return view;
     }
-
-    View.OnClickListener viewClickListener
-            = new View.OnClickListener(){
-
-        @Override
-        public void onClick(View view) {
-            // TODO Auto-generated method stub
-            showPopupMenu(view);
-        }
-
-    };
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -229,172 +155,15 @@ public class SurveyFragment extends Fragment{
         pd.show();
     }
 
-    /*private ActionMode.Callback modeCallBack = new ActionMode.Callback() {
-        // Called when the action mode is created; startActionMode() was called
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.setTitle("Actions");
-            mode.getMenuInflater().inflate(R.menu.action_survey_menu, menu);
-            return true;
-        }
 
-        // Called each time the action mode is shown.
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false; // Return false if nothing is done
-        }
-
-        // Called when the user selects a contextual menu item
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.menu_start:
-                    Toast.makeText(getContext(), "Start survey!", Toast.LENGTH_SHORT).show();
-                    mode.finish(); // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_stop:
-                    Toast.makeText(getContext(), "Stop survey!", Toast.LENGTH_SHORT).show();
-                    mode.finish(); // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_edit:
-                    Toast.makeText(getContext(), "Editing!", Toast.LENGTH_SHORT).show();
-                    mode.finish(); // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_delete:
-                    // Trigger the deletion here
-                    mode.finish(); // Action picked, so close the contextual menu
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        // Called when the user exits the action mode
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            currentActionMode = null; // Clear current action mode
-        }
-    };*/
-
-    public void popMenu(){
-        pd = new ProgressDialog(view.getContext());
-        pd.setContentView(R.layout.activity_login);
-        pd.setProgressStyle(android.app.ProgressDialog.STYLE_HORIZONTAL);
-        pd.setProgressStyle(android.app.ProgressDialog.STYLE_SPINNER);
-        pd.setCancelable(false);
-        pd.setCanceledOnTouchOutside(true);
-        pd.show();
-    }
-   private void displayPopupWindow(View anchorView) {
-        PopupWindow popup = new PopupWindow(view.getContext());
-
-        /*View layout = getLayoutInflater().inflate(R.layout.pop_menu, null);
-        popup.setContentView(layout);
-        // Set content width and height
-        popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-        popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
-        // Closes the popup window when touch outside of it - when looses focus
-        popup.setOutsideTouchable(true);
-        popup.setFocusable(true);
-        // Show anchored to button
-        popup.setBackgroundDrawable(new BitmapDrawable());
-        popup.showAsDropDown(anchorView);*/
-    }
-
-
-   /*  public boolean onMenuItemClick(MenuItem item) {
-        Toast.makeText(getContext(), "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
-        switch (item.getItemId()) {
-            case R.id.menu_start:
-                Toast.makeText(getContext(), "Start survey!", Toast.LENGTH_SHORT).show();
-                 // Action picked, so close the contextual menu
-                return true;
-            case R.id.menu_stop:
-                Toast.makeText(getContext(), "Stop survey!", Toast.LENGTH_SHORT).show();
-                 // Action picked, so close the contextual menu
-                return true;
-            case R.id.menu_edit:
-                Toast.makeText(getContext(), "Editing!", Toast.LENGTH_SHORT).show();
-                 // Action picked, so close the contextual menu
-                return true;
-            case R.id.menu_delete:
-                // Trigger the deletion here
-                 // Action picked, so close the contextual menu
-                return true;
-            default:
-                return false;
-        }
-    }*/
-
-    private void showPopupMenu(View v){
-        PopupMenu popupMenu = new PopupMenu(view.getContext(), v);
-        popupMenu.getMenuInflater().inflate(R.menu.action_survey_menu, popupMenu.getMenu());
-
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Toast.makeText(view.getContext(),
-                        item.toString(),
-                        Toast.LENGTH_LONG).show();
-                return true;
-            }
-        });
-
-        popupMenu.show();
-    }
-
-    private ActionMode.Callback modeCallBack = new ActionMode.Callback() {
-        // Called when the action mode is created; startActionMode() was called
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.setTitle("Actions");
-            mode.getMenuInflater().inflate(R.menu.action_survey_menu, menu);
-            return true;
-        }
-
-        // Called each time the action mode is shown.
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            return false; // Return false if nothing is done
-        }
-
-        // Called when the user selects a contextual menu item
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.menu_start:
-                    Toast.makeText(getContext(), "Start survey!", Toast.LENGTH_SHORT).show();
-                    // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_stop:
-                    Toast.makeText(getContext(), "Stop survey!", Toast.LENGTH_SHORT).show();
-                    // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_edit:
-                    Toast.makeText(getContext(), "Editing!", Toast.LENGTH_SHORT).show();
-                    // Action picked, so close the contextual menu
-                    return true;
-                case R.id.menu_delete:
-                    // Trigger the deletion here
-                    // Action picked, so close the contextual menu
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        // Called when the user exits the action mode
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            currentActionMode = null; // Clear current action mode
-        }
-    };
 
     private void showEditDialog() {
-        FragmentManager fm = getChildFragmentManager();
-        MenuFragment popMenuFragment = MenuFragment.newInstance("Some Title");
-        popMenuFragment.show(getActivity().getFragmentManager(), "po_menu");
+        /*FragmentManager fm = getChildFragmentManager();*/
+        MenuFragment pf = MenuFragment.newInstance("Some Title");
+        pf.show(getActivity().getFragmentManager(), "po_menu");
+
+
+
 
     }
 }
